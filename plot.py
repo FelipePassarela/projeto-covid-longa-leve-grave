9dc4300cca796545378c1109ae6b07fcd8302d75
FelipePassarela <126916744+FelipePassarela@users.noreply.github.com>
2024-08-26 07:13:19 -0300

    feat: Atualiza título do gráfico para AUC ROC

--- plot.py
+++ plot.py
@@ -2,6 +2,12 @@ import os
 import pandas as pd
 import matplotlib.pyplot as plt
 
+SCORE = "roc_auc"
+SCORE_MATCH = {
+    "accuracy": "Acurácia",
+    "f1": "F1",
+    "roc_auc": "ROC AUC"
+}
 
 def main():
     plt.figure(figsize=(10, 6))
@@ -10,19 +16,21 @@ def main():
         if file.endswith(".csv"):
             model_name = file.split("_")[0]
             data = pd.read_csv(os.path.join("results", file))
-            plt.plot(data["n_features"], data["roc_auc"], label=model_name, marker='o')
+            plt.plot(data["n_features"], data[SCORE], label=model_name, marker='o')
 
-    plt.title("AUC ROC para Diferentes Modelos")
+    score_title = SCORE_MATCH[SCORE]
+
+    plt.title(f"{score_title} para Diferentes Modelos")
     plt.xticks(data["n_features"])
     plt.xlabel("Número de Variantes (num_snp)")
-    plt.ylabel("AUC ROC")
+    plt.ylabel(score_title)
     plt.legend()
     plt.grid(True, which='both', linestyle='--')
     plt.tight_layout()
 
-    plt.savefig("plot.png")
+    os.makedirs("plots", exist_ok=True)
+    plt.savefig(f"plots/new_plot_{SCORE}.png")
     plt.show()
 
 if __name__ == "__main__":
     main()
-
