9ff101cc5f9bcb42f6a6cc3eb482999694a955c8
FelipePassarela <126916744+FelipePassarela@users.noreply.github.com>
2025-04-03 22:49:39 -0300

    Add ID formatting in merge

--- scripts/merge.py
+++ scripts/merge.py
@@ -21,12 +21,14 @@ def merge(spreadsheet_path: str, genom_path: str, merged_path: str, target_colum
         df_info = pd.read_csv(spreadsheet_path, usecols=['id', target_column])
         df_genom = pd.read_csv(genom_path)
 
-        # Ensure the id columns are equally formatted
-        df_info['id'] = df_info['id'].astype(str).str.strip()
-        df_genom['id'] = df_genom['id'].astype(str).str.strip()
+        format_ids(df_info, df_genom)
 
         df_merged = pd.merge(df_genom, df_info, on='id', how='left')
         df_merged = df_merged.dropna(subset=[target_column])
+
+        # for col in df_merged.columns:
+        #     if col != 'id':
+        #         df_merged[col] = df_merged[col].astype('category')
 
         missing_ids = set(df_info['id']) - set(df_merged['id'])
         print("Missing IDs in genomic dataset:", missing_ids)
@@ -38,17 +40,32 @@ def merge(spreadsheet_path: str, genom_path: str, merged_path: str, target_colum
         print(e)
 
 
+def format_ids(df_info: pd.DataFrame, df_genom: pd.DataFrame) -> None:
+    """
+    Format the IDs in the dataframes to ensure they match.
+
+    :param df_info: The patient information dataframe.
+    :param df_genom: The genomic data dataframe.    
+    """
+    df_info['id'] = df_info['id'].astype(str).str.strip()
+    df_genom['id'] = df_genom['id'].astype(str).str.strip()
+    df_genom['id'] = df_genom['id'].str.replace("UFES_", "")
+    df_genom['id'] = df_genom['id'].str.lstrip("0")
+    df_genom['id'] = df_genom['id'].str.replace("-", "_")
+
+
 if __name__ == "__main__":
+    genomic_path = "data/genomics/matriz_TESTE_4_AGORA.csv"
     datasets = [
-        ("data/grave/geral/planilha.csv", "data/genom_processed.csv", "data/grave/geral/merged.csv", "risk"),
-        ("data/grave/nao_vacinados/planilha.csv", "data/genom_processed.csv", "data/grave/nao_vacinados/merged.csv", "risk"),
+        ("data/grave/geral/planilha.csv", genomic_path, "data/grave/geral/merged.csv", "risk"),
+        ("data/grave/nao_vacinados/planilha.csv", genomic_path, "data/grave/nao_vacinados/merged.csv", "risk"),
 
-        ("data/longa/geral/planilha.csv", "data/genom_processed.csv", "data/longa/geral/merged.csv", "Long_COVID"),
-        ("data/longa/nao_vacinados/planilha.csv", "data/genom_processed.csv", "data/longa/nao_vacinados/merged.csv", "Long_COVID"),
+        ("data/longa/geral/planilha.csv", genomic_path, "data/longa/geral/merged.csv", "Long_COVID"),
+        ("data/longa/nao_vacinados/planilha.csv", genomic_path, "data/longa/nao_vacinados/merged.csv", "Long_COVID"),
 
-        ("data/dor/nao_vacinados/planilha.csv", "data/genom_processed.csv", "data/dor/nao_vacinados/merged.csv", "Pain_Block"),
-        ("data/sistema_cardiovascular/nao_vacinados/planilha.csv", "data/genom_processed.csv", "data/sistema_cardiovascular/nao_vacinados/merged.csv", "Cardiovascular_sequelae"),
-        ("data/sistema_nervoso/nao_vacinados/planilha.csv", "data/genom_processed.csv", "data/sistema_nervoso/nao_vacinados/merged.csv", "Sist_Nerv_Per_"),
+        ("data/dor/nao_vacinados/planilha.csv", genomic_path, "data/dor/nao_vacinados/merged.csv", "Pain_Block"),
+        ("data/sistema_cardiovascular/nao_vacinados/planilha.csv", genomic_path, "data/sistema_cardiovascular/nao_vacinados/merged.csv", "Cardiovascular_sequelae"),
+        ("data/sistema_nervoso/nao_vacinados/planilha.csv", genomic_path, "data/sistema_nervoso/nao_vacinados/merged.csv", "Sist_Nerv_Per_"),
     ]
     
     for spreadsheet_path, genom_path, save_path, target_column in datasets:
